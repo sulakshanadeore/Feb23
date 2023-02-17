3df6c4f3f6436699acf464d2eab731b683e384e9
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2023-02-17 10:55:23 +0530

    Add files via upload

---
+++ Products.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPs
+{
+    public class Products
+    {
+        static int cnt;
+        static Products()
+        {
+            cnt = 0;
+            Console.WriteLine("Thsi is called only once in the lifetime of the class, before any other constructor gets called... I m used for initializations");
+        }
+
+
+        public Products()
+        {
+            cnt += 1;
+            this.Productid = cnt;
+            Console.WriteLine("Default Constructor");
+        }
+
+        public Products(string pname,int p_qty)
+        {
+            cnt += 1;
+            this.Productid = cnt;
+            //this.Productid = pid;
+            this.ProductName = pname;
+            this.Qty = p_qty;
+        }
+        public Products(string pname,int p_qty,string city):this(pname,p_qty)
+        {
+            
+            
+            this.City = city;
+        }
+        string _city;
+        public string City
+        { get { return _city; } set { _city = value; } }
+
+
+        int prodid;//variable
+        public int Productid//property
+        {
+            //Accept value from user & assign to variable
+            set
+            {
+
+
+                prodid = value;
+                //if (value > 0)
+                //{
+                //    prodid = value;
+                //}
+                //else
+                //{
+                //    Console.WriteLine("Invalid product id...");
+                //}
+            }
+
+            
+            get { return prodid; }//return the value to the user
+
+        
+        }
+
+
+        private string prodname;
+
+        public string ProductName
+        {
+            get { return prodname; }
+            set 
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+
+                    prodname = value;
+
+                    if (prodname == "Chai")
+                    {
+                        _price = 10;
+                    }
+                    else if (prodname == "Coffee")
+                    {
+                        _price = 20;
+                    }
+                    else
+                    {
+
+                        _price = 30;
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Not a valid product");
+                }
+            }
+        }
+
+
+        //Read only property
+        int _price;
+        public int Price
+        {
+
+            get {
+            
+                return _price; 
+            }
+        }
+
+        int _qty;
+        public int Qty
+        {
+            set { _qty = value; }
+        
+        }
+
+        public float TotalPrice()
+        {
+            Console.WriteLine($"U ordered {_qty} at the price of Rs. {_price} so following is your amt");
+            return _price * _qty;
+        
+        }
+
+        public virtual double CalculateDiscount(float finalAmt)
+        {
+            double DiscountedAmt;
+            if ((finalAmt > 200000) && (finalAmt < 300000))
+            {
+                DiscountedAmt = finalAmt - (finalAmt * .01);
+            }
+            else if (finalAmt <= 200000)
+            {
+
+                DiscountedAmt = finalAmt;
+            }
+            else
+            {
+                DiscountedAmt = finalAmt - (finalAmt * .02);
+            }
+            return DiscountedAmt;
+
+        
+        
+        }
+        
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OOPs;
+namespace ExceptionHandlingDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a, b, c;
+            //string s;
+            try
+            {
+                //Console.WriteLine("Enter a");
+                //a = Convert.ToInt32(Console.ReadLine());
+                //Console.WriteLine("Enter b");
+                //b = Convert.ToInt32(Console.ReadLine());
+                //c = a / b;
+                //Console.WriteLine(c);
+
+                int[][] depts = new int[3][];
+                depts[0] = new int[5] { 10, 11, 12, 13, 14 };
+                depts[1] = new int[3] { 100, 200, 300 };
+                depts[2] = new int[10] { 1000, 2000, 3000 ,4000,5000,6000,7000,8000,9000,10000};
+              foreach (var item in depts)
+                {
+                    foreach (int i in item)
+                    {
+                        Console.Write(i+ " ");
+                    }
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine("------------------------------");
+                Products[] p = new Products[3];
+                p[0] = new Products("Chai", 20);
+                p[1] = new Products("Coffee", 30);
+                p[2] = new Products("Green tea", 50);
+
+                foreach (var item in p)
+                {
+                    Console.WriteLine(item.Productid);
+                    Console.WriteLine(item.ProductName);
+                    Console.WriteLine(item.Price);
+                     Console.WriteLine(item.TotalPrice());
+                }
+
+
+
+
+
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("This is called");
+                Console.WriteLine(ex.Message);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("this is format exception block");
+                Console.WriteLine(ex.Message);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("some other exception occured...");
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                //releasing the resources,
+                //closing the connections.
+                //closing the files etc.
+                //disposing objects created
+                Console.WriteLine("Finally block executed...");
+                Console.WriteLine("Pls visit again");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
