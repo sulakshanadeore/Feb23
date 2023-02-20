bb88f67df734321030c45fb3d4a870e1cb44931d
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2023-02-20 18:45:51 +0530

    SerilaizationDemo

---
+++ Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializationDemo
+{
+    [Serializable]
+    public class Employee
+    {
+        public int Empid { get; set; }
+
+        
+               
+        public string Empname { get; set; }
+        [NonSerialized]
+        float _sal;
+        public float Empsal { get { return _sal; } set { _sal = value; } }
+
+
+        private float Salary { get; set; }
+
+    }
+    
+}
--- Program.cs
+++ Program.cs
@@ -1,68 +1,104 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
 
-
-namespace FilesDemo
+namespace SerializationDemo
 {
     class Program
     {
+        void BinaryDeserializeData()
+        {
+            //using System.Runtime.Serialization.Formatters.Binary;
+            FileStream fs = new FileStream("EmpData.dat", FileMode.Open, FileAccess.Read);
+
+            BinaryFormatter bf = new BinaryFormatter();
+            Employee emp1=(Employee)bf.Deserialize(fs);
+            Console.WriteLine(emp1.Empid);
+            Console.WriteLine(emp1.Empname);
+            Console.WriteLine(emp1.Empsal);
+        
+        }
+
+
+        void XMLSerializeData()
+        {
+            Employee emp = new Employee();
+            emp.Empid = 101;
+            emp.Empname = "Aditi";
+            
+            FileStream fs = new FileStream("EmpXMLData.xml", FileMode.Create, FileAccess.Write);
+            XmlSerializer xs = new XmlSerializer(typeof(Employee));
+            xs.Serialize(fs, emp);
+            fs.Close();
+            fs.Dispose();
+
+            //using System.Xml.Serialization;
+
+
+        }
+
+        void XMLDeSerializeData()
+        {
+            FileStream fs = new FileStream("EmpXMLData.xml", FileMode.Open, FileAccess.Read);
+            XmlSerializer xs = new XmlSerializer(typeof(Employee));
+            Employee emp=(Employee)xs.Deserialize(fs);
+            Console.WriteLine(emp.Empid);
+            Console.WriteLine(emp.Empname);
+            Console.WriteLine(emp.Empsal);
+            fs.Close();
+            fs.Dispose();
+
+
+            //using System.Xml.Serialization;
+        }
+
+
+
+        void BinarySerializeData()
+        {
+            //using System.Runtime.Serialization.Formatters.Binary;
+            Employee emp = new Employee();
+            emp.Empid = 101;
+            emp.Empname = "Aditi";
+            emp.Empsal = 40000.00f;
+            FileStream fs = new FileStream("EmpData.dat", FileMode.Create, FileAccess.Write);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(fs, emp);
+            fs.Close();
+            fs.Dispose();
+
+            Console.WriteLine("file created in binary format....");
+        }
+
+
         static void Main(string[] args)
         {
-            const string path = @"D:\Sulakshana\StudyMaterial\Feb23\FilesDemo\FilesDemo\MyFiles\";
+            Console.WriteLine("enter 1 for Binaryserialize, 2 for Binarydeserialize, 3 for XML Serialze,  4 for XML Deserialize");
+            int choice = int.Parse(Console.ReadLine());
+            Program p = new Program();
+            switch (choice)
+            {
+                
+                case 1:
+                    p.BinarySerializeData();
+                    break;
 
-            const string path1 = @"D:\Sulakshana\StudyMaterial\Feb23\FilesDemo\FilesDemo\Folder1\";
-            string filename = FileCreation(path);
+                case 2:
+                    p.BinaryDeserializeData();
+                    break;
+                case 3:
+                    p.XMLSerializeData();
 
-            //string filename = "cv.txt";
-
-            string p = string.Concat(path, filename);
-
-
-            //File.WriteAllText(p, "this is my second demo file created today");
-            //string[] contents = new string[1]{"This is a file created usign string array the method  used is WriteAllLines" };
-            //File.WriteAllLines(p, contents);
-            //Console.WriteLine("file created... ");
-            // File.Delete(p);
-            //Console.WriteLine("file deleted... ");
-            //File.AppendAllLines(p,)
-            //File.AppendAllText(p, "\n This is appended lines of text.....");
-
-            CopyFile(p);
-            MoveFile(p);
-
+                    break;
+                case 4:
+                    p.XMLDeSerializeData();
+                    break;
+                default:
+                    break;
+            }
+            
             Console.ReadLine();
-
-        }
-
-        private static void MoveFile(string p)
-        {
-            const string path1 = @"D:\Sulakshana\StudyMaterial\Feb23\FilesDemo\FilesDemo\NewFolder\";
-
-            string p2 = string.Concat(path1, "c1.txt");
-            File.Move(p, p2);
-            Console.WriteLine("File Moved....");
-        }
-
-        private static void CopyFile(string p)
-        {
-            const string path1 = @"D:\Sulakshana\StudyMaterial\Feb23\FilesDemo\FilesDemo\NewFolder\";
-
-
-            string p2 = string.Concat(path1, "c1.txt");
-            File.Copy(p, p2);
-            Console.WriteLine("File copied....");
-        }
-
-        private static string FileCreation(string path)
-        {
-            Console.WriteLine("Enter the filename");
-            string filename = Console.ReadLine().Trim();
-            File.Create(string.Concat(path, filename));
-            return filename;
         }
     }
 }
