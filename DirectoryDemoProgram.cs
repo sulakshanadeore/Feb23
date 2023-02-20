b4f1c390a10a56d2538631dfa056427826060e9d
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2023-02-20 14:52:59 +0530

    Add files via upload

---
+++ DirectoryDemoProgram.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FilesDemo
+{
+    class DirectoryDemoProgram
+    {
+        static void Main(string[] args)
+        {
+
+            string[] drives=Directory.GetLogicalDrives();
+
+
+            foreach (var info1    in drives)
+            {
+                Console.WriteLine(info1);
+
+                Console.WriteLine();
+            }
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("C drives info");
+            DriveInfo info = new DriveInfo("C");
+            Console.WriteLine("Name=" + info.Name);
+            Console.WriteLine("Volumne Label= " + info.VolumeLabel);
+            Console.WriteLine("Drive Format  " + info.DriveFormat);
+            Console.WriteLine("Drive Type= " + info.DriveType);
+            Console.WriteLine("Root Dir" + info.RootDirectory);
+            Console.WriteLine();
+
+            Console.WriteLine("D drives info");
+            info = new DriveInfo("D");
+            Console.WriteLine("Name=" + info.Name);
+            Console.WriteLine("Volumne Label= " + info.VolumeLabel);
+            Console.WriteLine("Drive Format  " + info.DriveFormat);
+            Console.WriteLine("Drive Type= " + info.DriveType);
+            Console.WriteLine("Root Dir" + info.RootDirectory);
+            Console.WriteLine();
+
+            Console.WriteLine("E drives info");
+             info = new DriveInfo("E");
+            Console.WriteLine("Name=" + info.Name);
+            Console.WriteLine("Volumne Label= " + info.VolumeLabel);
+            Console.WriteLine("Drive Format  " + info.DriveFormat);
+            Console.WriteLine("Drive Type= " + info.DriveType);
+            Console.WriteLine("Root Dir" + info.RootDirectory);
+            //const string path= @"D:\Sulakshana\StudyMaterial\Feb23\FilesDemo\FilesDemo\";
+            //string dirname = "Students";
+            //string s=string.Concat(path, dirname);
+            //string destpath = string.Concat(path, dirname+ "\\");
+            //Console.WriteLine("Destination Path"+ destpath);
+            //Console.WriteLine(  );
+            //string[] filenames=Directory.GetFiles(@"D:\Sulakshana\StudyMaterial\Feb23\FilesDemo\FilesDemo\MyFiles\");
+
+            //foreach (string item in filenames)
+            //{
+
+            //    Console.WriteLine("FileName=" + item);
+
+            //}
+
+
+
+            //foreach (var item in filenames)
+            //{
+
+            //   File.Copy(item, string.Concat(destpath, "file1.txt"));
+
+
+
+            //}
+
+
+            //Directory.CreateDirectory(s);
+            //Console.WriteLine("Created Dir");
+            //string subdir=string.Concat(s,  "\\CourseInfo");
+            //Directory.CreateDirectory(subdir);
+
+            //Console.WriteLine("Sub  directory created....");
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace FilesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string path = @"D:\Sulakshana\StudyMaterial\Feb23\FilesDemo\FilesDemo\MyFiles\";
+
+            const string path1 = @"D:\Sulakshana\StudyMaterial\Feb23\FilesDemo\FilesDemo\Folder1\";
+            string filename = FileCreation(path);
+
+            //string filename = "cv.txt";
+
+            string p = string.Concat(path, filename);
+
+
+            //File.WriteAllText(p, "this is my second demo file created today");
+            //string[] contents = new string[1]{"This is a file created usign string array the method  used is WriteAllLines" };
+            //File.WriteAllLines(p, contents);
+            //Console.WriteLine("file created... ");
+            // File.Delete(p);
+            //Console.WriteLine("file deleted... ");
+            //File.AppendAllLines(p,)
+            //File.AppendAllText(p, "\n This is appended lines of text.....");
+
+            CopyFile(p);
+            MoveFile(p);
+
+            Console.ReadLine();
+
+        }
+
+        private static void MoveFile(string p)
+        {
+            const string path1 = @"D:\Sulakshana\StudyMaterial\Feb23\FilesDemo\FilesDemo\NewFolder\";
+
+            string p2 = string.Concat(path1, "c1.txt");
+            File.Move(p, p2);
+            Console.WriteLine("File Moved....");
+        }
+
+        private static void CopyFile(string p)
+        {
+            const string path1 = @"D:\Sulakshana\StudyMaterial\Feb23\FilesDemo\FilesDemo\NewFolder\";
+
+
+            string p2 = string.Concat(path1, "c1.txt");
+            File.Copy(p, p2);
+            Console.WriteLine("File copied....");
+        }
+
+        private static string FileCreation(string path)
+        {
+            Console.WriteLine("Enter the filename");
+            string filename = Console.ReadLine().Trim();
+            File.Create(string.Concat(path, filename));
+            return filename;
+        }
+    }
+}
