0b7024b68ad4684d96c523ec46fc3a8648b4f796
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2023-02-23 12:51:43 +0530

    Add files via upload

---
+++ EmployeeAlreadyExists.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLibrary
+{
+
+    [Serializable]
+    public class EmployeeAlreadyExistsException : Exception
+    {
+        public EmployeeAlreadyExistsException() { }
+        public EmployeeAlreadyExistsException(string message) : base(message) { }
+        public EmployeeAlreadyExistsException(string message, Exception inner) : base(message, inner) { }
+        protected EmployeeAlreadyExistsException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
