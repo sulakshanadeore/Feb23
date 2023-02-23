3993f1b1bfb2ba74987a587ea3a22be3fc262260
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2023-02-23 12:45:36 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLibrary
+{
+    public class Employee
+    {
+              
+
+        public int Empid { get; set; }
+        string _ename;
+        public string EName { 
+            get 
+            {
+                return _ename;
+            
+            } 
+            
+            
+            set {
+                if ((value.Length>0) && (!string.IsNullOrEmpty(value)))
+                {
+                    _ename = value;
+                }
+                else
+                {
+                    throw new FormatException("Ename cannot be empty");
+                }
+
+            } 
+            
+        
+        }
+        public int Deptno { get; set; }
+
+
+        
+        
+
+    }
+}
---
+++ EmployeeMgt.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRLibrary
+{
+    public class EmployeeMgt
+    {
+
+        public static List<Employee> emplist = new List<Employee>();
+        public EmployeeMgt()
+        {
+
+            emplist.Add(new Employee { Empid = 1, EName = "Akshit", Deptno = 10 });
+            emplist.Add(new Employee { Empid = 2, EName = "Jim", Deptno = 20 });
+            emplist.Add(new Employee { Empid = 3, EName = "Mary", Deptno = 10 });
+            emplist.Add(new Employee { Empid = 4, EName = "Raj", Deptno = 20 });
+
+
+        }
+
+
+        Employee empfound;
+        public void Findemployee(int empno)
+        {
+
+            if ((empno != 0) && empno > 0)
+            {
+              empfound = emplist.Find(e => e.Empid == empno);
+                if (empfound != null)
+                {
+                    Console.WriteLine(empfound.Empid);
+                    Console.WriteLine(empfound.EName);
+                    Console.WriteLine(empfound.Deptno);
+                }
+
+
+            }
+            else
+            {
+                throw new EmployeeNotFoundException("Pls check the empno you trying to find.....");
+            }
+
+
+        }
+
+        public void AddEmployee(Employee emp)
+        {
+            Employee e1 = emplist.Find(e => e.Empid == emp.Empid);
+
+            if (e1==null)
+            {
+                emplist.Add(new Employee { Empid = emp.Empid, EName = emp.EName, Deptno = emp.Deptno });
+                
+            }
+            else
+            {
+             
+                throw new EmployeeAlreadyExistsException("this record already exists");
+            }
+
+        }
+        public void DeleteEmployee(int empno)
+        {
+            Findemployee(empno);
+            emplist.Remove(empfound);
+
+
+
+
+        }
+    }
+}
